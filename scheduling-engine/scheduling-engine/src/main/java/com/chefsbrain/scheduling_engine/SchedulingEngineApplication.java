1abacfff842d7f985099036b4e30c83d4c00ad54
Niviru I.U. Weerasinghe <niviruedu2006@gmail.com>
2026-01-27 22:48:10 +0530

    Backend initialized Please enter the commit message for your changes. Lines starting

---
+++ scheduling-engine/scheduling-engine/src/main/java/com/chefsbrain/scheduling_engine/SchedulingEngineApplication.java
@@ -0,0 +1,13 @@
+package com.chefsbrain.scheduling_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingEngineApplication.class, args);
+	}
+
+}
---
+++ scheduling-engine/scheduling-engine/src/test/java/com/chefsbrain/scheduling_engine/SchedulingEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chefsbrain.scheduling_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
